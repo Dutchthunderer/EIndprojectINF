256813530aaec5143967b733fd18e6f849af6a6a
FlipDark <sanderbenning2007@gmail.com>
2024-02-28 13:30:26 +0100

    transitie gameplay
    koppeling gemaakt van hoofdmenu naar de gameplay

--- menu.gd
+++ menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 
 func _on_options_pressed():
