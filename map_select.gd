110624cc78412d169cbab805b6cc89562a582d08
DutchThunderer <timo.zwerver@gmail.com>
2024-03-20 14:43:05 +0100

    collission toegevoegd aan testmap

--- map_select.gd
+++ map_select.gd
@@ -2,3 +2,8 @@ extends Control
 
 func _on_lvl_1_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+
+func _on_lvl_2_pressed():
+	get_tree().change_scene_to_file("res://timoracist.tscn")
