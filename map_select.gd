c3b839e1bd1c4468bfcd2d95a89f3e56d89d3318
DutchThunderer <timo.zwerver@gmail.com>
2024-04-04 21:06:19 +0200

    spel af eindelijk

--- map_select.gd
+++ map_select.gd
@@ -1,9 +1,9 @@
 extends Control
 
 func _on_lvl_1_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://testmap.tscn")
 
 
 
 func _on_lvl_2_pressed():
-	get_tree().change_scene_to_file("res://testmap.tscn")
+	get_tree().change_scene_to_file("res://level2.tscn")
